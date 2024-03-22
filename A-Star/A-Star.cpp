f7ca4ae9c891a6a11f3ec6bfdffeea4b677487a8
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2024-03-22 11:08:40 +0100

    Raylib Project Creation

--- A-Star/A-Star.cpp
+++ A-Star/A-Star.cpp
@@ -5,12 +5,15 @@
 #include <vector>
 #include <limits>
 
+enum NodeType {Ground = 0, Wall = 1, Challenging = 2, Dangerous = 3};
+
 #define inf std::numeric_limits<float>::max()
 struct Node {
 public:
     float g, h, f;
     Node* Parent;
     int x, y;
+    NodeType type;
 };
 
 std::vector<Node*> openList = std::vector<Node*>();
@@ -18,10 +21,10 @@ std::vector<Node*> closedList = std::vector<Node*>();
 
 std::vector<std::vector<int>> map = {
     {0,0,0,0,0},
-    {0,1,1,0,0},
-    {0,0,1,1,1},
-    {1,0,1,0,0},
-    {0,0,0,0,0}
+    {0,1,1,1,3},
+    {0,0,1,1,3},
+    {1,3,1,3,3},
+    {1,3,3,3,3}
 };
 
 std::vector<std::vector<Node*>> nodeMap(5,std::vector<Node*>(5, new Node()));
@@ -31,13 +34,16 @@ int getIndexFromValue(std::vector<Node*>& list, Node* v);
 Node* searchMin(std::vector<Node*>& list);
 std::vector<Node*> setChildren(Node* n);
 int ifIsInListViaPos(Node* value, std::vector<Node*> list);
+bool hasSamePos(Node* n1, Node* n2);
 float getDist(Node* n1, Node* n2);
 bool ifIsInList(Node* value, std::vector<Node*> list);
+float getNodeTypeMultiplier(NodeType t);
+void showPath(Node* start, Node* end, std::vector<std::vector<Node*>> list);
 
 int main()
 {
     initNodeMap();
-    Node* startNode = nodeMap[0][4];
+    Node* startNode = nodeMap[1][2];
     startNode->f = 0;
     Node* endNode = nodeMap[4][0];
 
@@ -50,17 +56,17 @@ int main()
         closedList.push_back(currentNode);
 
         if (currentNode == endNode) {
-            std::cout << "GGs you won";
+            std::cout << "GGs you won" << std::endl;
+            showPath(startNode, endNode, nodeMap);
             return 0;
         }
-        std::vector<Node*> childNodes = {};
-        setChildren(currentNode);
+        std::vector<Node*> childNodes = setChildren(currentNode);
         for (int i = 0; i < childNodes.size(); i++) {
             Node* childNode = childNodes[i];
             if (ifIsInList(childNode, closedList)) {
                 continue;
             }
-            childNode->g = currentNode->g + getDist(currentNode, childNode);
+            childNode->g = currentNode->g + getDist(currentNode, childNode) * getNodeTypeMultiplier(childNode->type);
             childNode->h = getDist(childNode, endNode);
             childNode->f = childNode->g + childNode->h;
 
@@ -73,6 +79,7 @@ int main()
             openList.push_back(childNode);
         }
     }
+    
 }
 
 void initNodeMap() {
@@ -84,6 +91,9 @@ void initNodeMap() {
             n->g = 0;
             n->h = 0;
             n->f = n->g + n->h;
+            n->Parent = nullptr;
+
+            n->type = static_cast<NodeType>(map[y][x]);
             nodeMap[x][y] = n;
         }
     }
@@ -115,13 +125,17 @@ Node* searchMin(std::vector<Node*>& list) {
 }
 
 std::vector<Node*> setChildren(Node* n) {
-    std::vector<Node*> children = {};
+    std::vector<Node*> children = std::vector<Node*>();
     for (int x = -1; x < 2; x++) {
-        if ((n->x + x > 0) && (n->x + x < nodeMap[0].size())) {
+        if ((n->x + x >= 0) && (n->x + x < nodeMap[0].size())) {
             for (int y = -1; y < 2; y++) {
-                if ((n->y + y > 0) && (n->y + y < nodeMap[0].size())) {
-                    nodeMap[x][y]->Parent = n;
-                    children.push_back(n);
+                if ((n->y + y >= 0) && (n->y + y < nodeMap[0].size())) {
+                    int xP = n->x + x;
+                    int yP = n->y + y;
+                    if (!(x == 0 && y == 0) && nodeMap[xP][yP]->type != NodeType::Wall && (ifIsInListViaPos(nodeMap[xP][yP],closedList)==-1)) {
+                        nodeMap[xP][yP]->Parent = n;
+                        children.push_back(nodeMap[xP][yP]);
+                    }
                 }
             }
         }
@@ -140,11 +154,40 @@ bool ifIsInList(Node* value, std::vector<Node*> list) {
 
 int ifIsInListViaPos(Node* value, std::vector<Node*> list) {
     for (int i = 0; i < list.size(); i++) {
-        if (list[i]->x == value->x && list[i]->y == value->y) {
+        if (hasSamePos(list[i],value)) {
             return i;
         }
     }
     return -1;
+}
+
+bool hasSamePos(Node* n1, Node* n2)
+{
+    return n1->x == n2->x && n1->y == n2->y;
+}
+
+void showPath(Node* start, Node* end, std::vector<std::vector<Node*>> list) {
+    Node* n = list[end->x][end->y];
+    std::cout << "Path : " << std::endl;
+    while (!hasSamePos(n,start)) {
+        std::cout << "Position : x =" << n->x << " | y = " << n->y << std::endl;
+        Node* p = n->Parent;
+        n = list[p->x][p->y];
+    }
+    std::cout << "Position : x =" << n->x << " | y = " << n->y << std::endl;
+}
+
+float getNodeTypeMultiplier( NodeType t ){
+    switch (t) {
+    case NodeType::Ground:
+        return 1;
+    case NodeType::Wall:
+        return inf;
+    case NodeType::Challenging:
+        return 1.5;
+    case NodeType::Dangerous:
+        return 2;
+    }
 }
 
 
